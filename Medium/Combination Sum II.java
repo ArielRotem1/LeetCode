cfe4b7f0fbb94ca0152621259f73f9c8e8cf95e9
Ariel Rotem <73435828+ArielRotem1@users.noreply.github.com>
2021-10-11 20:44:04 +0300

    Create Combination Sum II.java

---
+++ Medium/Combination Sum II.java
@@ -0,0 +1,29 @@
+
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] cand, int target) {
+        Arrays.sort(cand);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> path = new ArrayList<Integer>();
+        DFS(cand, 0, target, path, res);
+        return res;
+    }
+    
+    private void DFS(int[] cand, int cur, int target, List<Integer> path, List<List<Integer>> res){
+        
+        if (target == 0) {
+            res.add(new ArrayList(path));
+            return;
+        }
+        
+        if (target < 0) return;
+        
+        for (int i = cur; i < cand.length; i++){
+            if (i > cur && cand[i] == cand[i-1]) continue;
+            
+            path.add(cand[i]);
+            DFS(cand, i+1, target - cand[i], path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
