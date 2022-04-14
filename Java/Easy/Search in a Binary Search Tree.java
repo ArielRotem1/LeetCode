bd8c9759f6f915ed2999c996afea8d362998f5f3
Ariel Rotem <73435828+ArielRotem1@users.noreply.github.com>
2022-04-14 10:42:54 +0300

    Create Search in a Binary Search Tree.java

---
+++ Java/Easy/Search in a Binary Search Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        if(root.val == val) return root;
+        
+        TreeNode resLeft = searchBST(root.left, val);
+        
+        if(resLeft != null) return resLeft;
+        
+        TreeNode resRight = searchBST(root.right, val);
+        
+        return resRight;
+    }
+}
